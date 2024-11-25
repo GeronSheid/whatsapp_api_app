7b550508da10c4c351bab4658296b3a5d87fbef4
Dmitry Shneider <dmitryshneider@gmail.com>
2024-11-25 10:41:38 +0600

    Исправил поведение скролла в окне чата

--- src/components/ChatWindowMessages/ChatWindowMessages.tsx
+++ src/components/ChatWindowMessages/ChatWindowMessages.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './styles.module.scss';
 import { IMessage } from '../../shared/store/store';
 import Message from '../Message/Message';
@@ -8,8 +8,15 @@ interface ChatWindowMessagesProps {
 }
 
 const ChatWindowMessages: React.FC<ChatWindowMessagesProps> = ({ messages }) => {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const endRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
     return (
-        <div className={styles.window}>
+        <div className={styles.window} ref={containerRef}>
             {messages?.map(message =>
                 <Message
                     key={message.messageId}
@@ -18,6 +25,7 @@ const ChatWindowMessages: React.FC<ChatWindowMessagesProps> = ({ messages }) => 
                     chatId={message.chatId}
                     sender={message?.sender} 
                 />)}
+                <div ref={endRef} />
         </div>
     )
 }
